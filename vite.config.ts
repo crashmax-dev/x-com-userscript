15951e3c437b8594d966e951e7afb809f9d432c4
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-07-27 22:15:05 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import Userscript from 'vite-userscript-plugin'
+
+import { author, homepage, license, name, version } from './package.json'
+
+export default defineConfig((config) => {
+  return {
+    plugins: [
+      Userscript({
+        fileName: 'vite-userscript-template',
+        entry: 'src/index.ts',
+        header: {
+          name,
+          version,
+          author,
+          license,
+          homepage,
+          match: ['http://localhost:3000', 'https://example.com']
+        },
+        server: {
+          port: 3000
+        }
+      })
+    ]
+  }
+})
