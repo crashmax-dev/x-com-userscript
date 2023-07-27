7527c7897256c76b4d140ecb806618a99fa33795
crashmax-dev <crashmax.dev@gmail.com>
2023-07-27 15:25:00 +0000

    Deploying to gh-pages from @ crashmax-dev/x-com-userscript@08b6875a2188a54e1c26a53564ac3eb7bd821a5d 🚀

--- x-com-userscript.meta.js
+++ x-com-userscript.meta.js
@@ -4,10 +4,11 @@
 // @author      crashmax <userscript@crashmax.ru>
 // @license     MIT
 // @homepage    https://crashmax-dev.github.io/x-com-userscript/
+// @connect     crashmax-dev.github.io
 // @icon        https://abs.twimg.com/favicons/twitter.3.ico
 // @match       *://twitter.com/*
 // @match       *://x.com/*
-// @grant       GM_addStyle
+// @grant       GM_xmlhttpRequest
 // @updateURL   https://crashmax-dev.github.io/x-com-userscript/x-com-userscript.meta.js
 // @downloadURL https://crashmax-dev.github.io/x-com-userscript/x-com-userscript.user.js
 // ==/UserScript==
